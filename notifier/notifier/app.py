9cf46d10d809a990d79419606a6e82eaefe8a568
Will McGugan <willmcgugan@gmail.com>
2015-12-15 21:49:11 +0000

    tidy

--- notifier/notifier/app.py
+++ notifier/notifier/app.py
@@ -9,25 +9,26 @@ from . import server
 
 log = logging.getLogger('notifier')
 
+
 def main():
-	parser = argparse.ArgumentParser(description='Run a notify server.')
-	parser.add_argument('--host', dest='host', metavar='IP',
-						help='server IP')
-	parser.add_argument('--port', dest='port', metavar='PORT', default=7474, type=int,
-						help='server port')
-	parser.add_argument('-s', '--secret', dest='secret', metavar='SECRET_TEXT', default="secret",
-						help='server secret (password)')
+    parser = argparse.ArgumentParser(description='Run a notify server.')
+    parser.add_argument('--host', dest='host', metavar='IP',
+                        help='server IP')
+    parser.add_argument('--port', dest='port', metavar='PORT', default=7474, type=int,
+                        help='server port')
+    parser.add_argument('-s', '--secret', dest='secret', metavar='SECRET_TEXT', default="secret",
+                        help='server secret (password)')
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	logging.basicConfig(format='%(asctime)s %(message)s',
-						level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s %(message)s',
+                        level=logging.DEBUG)
 
-	app = server.make_app(args.secret)
-	
-	app.listen(args.port)
-	log.info('listening on port %s', args.port)
-	try:
-		ioloop.IOLoop.instance().start()
-	except:
-		log.info('user exit')
+    app = server.make_app(args.secret)
+
+    app.listen(args.port)
+    log.info('listening on port %s', args.port)
+    try:
+        ioloop.IOLoop.instance().start()
+    except:
+        log.info('user exit')
