f0c9418b13bf9572ac3792b96cb838ca89d3a28f
Will McGugan <willmcgugan@gmail.com>
2016-01-03 12:13:21 +0000

    refactor timeline.js in to a widget + jquery plugin

--- willmcgugan.timeline/media/js/timeline.js
+++ willmcgugan.timeline/media/js/timeline.js
@@ -47,153 +47,167 @@ function Watcher(url, on_instructions)
 	return self;
 }
 
-function Stream(events, time)
-{
-	var self = this;
-	self.event_source = events;
-	self.time = time;
 
-	self.event_stack = [];
+(function($) {
+    $.fn.inthingStream = function(config) {
+		var self = this;
+		var rpc = new JSONRPC(config.rpc_url);
 
-
-	self.update_events = function(time, events)
-	{
-		self.time = time;
-		$(events).each(function(i, event_update){
-			self.add_event(event_update);
-		});
-		self.check_updates();
-	}
-
-	self.add_event = function(event_update)
-	{
-		self.event_stack.push(event_update);
-	}
-
-	self.check_updates = function()
-	{
-		if(!self.event_stack.length)
-		{
-			return;
-		}
-		$(self.event_stack).each(function(i, event_update){
-
-			//var $event = $(event_update.html);
-			var $existing_event = $('#event-' + event_update.id);
-			if (!$existing_event.length)
+		var $subscribe_button = $('.subscribe-button');
+		$('.subscribe-button').click(function(event){
+			if ($subscribe_button.hasClass('unsubscribed'))
 			{
-				var $timeline_container = $('.timeline-container');
-				$(event_update.html).prependTo($timeline_container);
+				rpc.call(
+					'stream.subscribe',
+					{'stream': stream_id},
+					function(result){
+						$subscribe_button.removeClass('unsubscribed').addClass('subscribed');
+					});
+			}
+			else
+			{
+				rpc.call(
+					'stream.unsubscribe',
+					{'stream': stream_id},
+					function(result){
+						$subscribe_button.removeClass('subscribed').addClass('unsubscribed');
+					});
+			}
+		});
+
+		self.event_source = config.source;
+		self.time = config.time;
+
+		self.event_stack = [];
+
+
+		self.update_events = function(time, events)
+		{
+			self.time = time;
+			$(events).each(function(i, event_update){
+				self.add_event(event_update);
+			});
+			self.check_updates();
+		}
+
+		self.add_event = function(event_update)
+		{
+			self.event_stack.push(event_update);
+		}
+
+		self.check_updates = function()
+		{
+			if(!self.event_stack.length)
+			{
+				return;
+			}
+
+			$window = $(window);
+			var scroll_y = $window.scrollTop();
+
+			if (scroll_y > 100)
+			{
+				$('new-events').html('<b>' + self.event_stack.length + '</b> new events');
+				return;
+			}
+
+			$(self.event_stack).each(function(i, event_update){
+
+				//var $event = $(event_update.html);
 				var $existing_event = $('#event-' + event_update.id);
-				$existing_event.addClass('new-event');
-			}
-            self.event_stack = [];
-		});
-		setTimeout(function(){
-			$('.event').removeClass('new-event');
-		}, 50);
-
-	}
-
-	self.check_append_events = function()
-	{
-		var $more_events = $('.more-events');
-
-		if(!$more_events.length || $more_events.hasClass('loading'))
-		{
-			return;
-		}
-
-		var $events = $('.timeline-container .event');
-		var $last_event = $($events[$events.length - 1]);
-		var event_data = $last_event.data();
-		last_event_time = event_data.time;
-
-		$more_events.addClass('loading');
-
-		rpc.call(
-			'events.get_updates',
-			{'time': last_event_time, 'events': self.event_source, 'new': false},
-			function(result){
-				$more_events.removeClass('loading');
-				if(!result.events.length)
+				if (!$existing_event.length)
 				{
-					$more_events.remove();
-					return;
+					var $timeline_container = $('.timeline-container');
+					$(event_update.html).prependTo($timeline_container);
+					var $existing_event = $('#event-' + event_update.id);
+					$existing_event.addClass('new-event');
 				}
-				$(result.events).each(function(i, event){
-					if(!$('#event-' + result.uuid).length)
-					{
-						$more_events.before(event.html);
-					}
-				});
+	            self.event_stack = [];
 			});
-	}
+			setTimeout(function(){
+				$('.event').removeClass('new-event');
+			}, 50);
 
- 	self.update = function()
-	{
-		rpc.call(
-			'events.get_updates',
-			{'time': self.time, 'events': self.event_source},
-			function(result){
-				self.update_events(result.time, result.events);
-			});
-	}
+		}
 
+		self.check_append_events = function()
+		{
+			var $more_events = $('.more-events');
 
-	var on_instructions = function(instructions)
-	{
-		$(instructions).each(function(i, instruction){
-			var action = instruction.action;
-			if(action=='update-stream')
+			if(!$more_events.length || $more_events.hasClass('loading'))
 			{
-				stream.update();
+				return;
+			}
+
+			var $events = $('.timeline-container .event');
+			var $last_event = $($events[$events.length - 1]);
+			var event_data = $last_event.data();
+			last_event_time = event_data.time;
+
+			$more_events.addClass('loading');
+
+			rpc.call(
+				'events.get_updates',
+				{'time': last_event_time, 'events': self.event_source, 'new': false},
+				function(result){
+					$more_events.removeClass('loading');
+					if(!result.events.length)
+					{
+						$more_events.remove();
+						return;
+					}
+					$(result.events).each(function(i, event){
+						if(!$('#event-' + result.uuid).length)
+						{
+							$more_events.before(event.html);
+						}
+					});
+				});
+		}
+
+	 	self.update = function()
+		{
+			rpc.call(
+				'events.get_updates',
+				{'time': self.time, 'events': self.event_source},
+				function(result){
+					self.update_events(result.time, result.events);
+				});
+		}
+
+
+		var on_instructions = function(instructions)
+		{
+			$(instructions).each(function(i, instruction){
+				var action = instruction.action;
+				if(action=='update-stream')
+				{
+					stream.update();
+				}
+			});
+		}
+
+		$(window).scroll(function(e){
+			var $more_events = $('.more-events');
+			if(!$more_events.length)
+			{
+				return;
+			}
+			var window_height = $(window).height();
+			var more_y = $more_events.offset().top;
+			var scroll_y = $(window).scrollTop();
+			
+			if (more_y - (scroll_y + window_height) <= 0)
+			{
+				self.check_append_events();
 			}
 		});
+
+		self.watcher = new Watcher(config.watcherurl, on_instructions);
+		self.watcher.connect();
 	}
+})(jQuery);
 
-	var $body = $('body');
-	var data = $body.data();
-
-	self.watcher = new Watcher(data.watcherurl, on_instructions);
-	self.watcher.connect()
-
-
-	return this;
-}
-
-$(function(){
-	var $body = $('body');
-	var data = $body.data();
-
-	rpc = new JSONRPC(data.rpc_url);
-	stream = new Stream(data.events, data.time);
-    stream_id = data.stream;
-
-	var $subscribe_button = $('.subscribe-button');
-
-	$('.subscribe-button').click(function(event){
-		if ($subscribe_button.hasClass('unsubscribed'))
-		{
-			rpc.call(
-				'stream.subscribe',
-				{'stream': stream_id},
-				function(result){
-					$subscribe_button.removeClass('unsubscribed').addClass('subscribed');
-				});
-		}
-		else
-		{
-			rpc.call(
-				'stream.unsubscribe',
-				{'stream': stream_id},
-				function(result){
-					$subscribe_button.removeClass('subscribed').addClass('unsubscribed');
-				});
-		}
-	});
-
-});
 
 $(function(){
     $('.moya-comment-form-editor').each(function(i, el){
@@ -260,22 +274,3 @@ $(function(){
 });
 
 
-$(function() {
-	var $more_events = $('.more-events');
-	var $window = $(window);
-	if (!$more_events.length)
-	{
-		return;
-	}
-	$(window).scroll(function(e){
-		var $more_events = $('.more-events');
-		var window_height = $(window).height();
-		var more_y = $more_events.offset().top;
-		var scroll_y = $(window).scrollTop();
-		
-		if (more_y - (scroll_y + window_height) <= 0)
-		{
-			stream.check_append_events();
-		}
-	});
-});
