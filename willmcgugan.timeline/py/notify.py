30a4e663c3dbb0d1be18b693bfa26ae04fb4b5de
Will McGugan <willmcgugan@gmail.com>
2016-02-24 22:17:38 +0000

    tidy notify code, cache event template fix

--- willmcgugan.timeline/py/notify.py
+++ willmcgugan.timeline/py/notify.py
@@ -12,26 +12,22 @@ import logging
 
 log = logging.getLogger('moya.runtime.notifier')
 
+XMLNS = "http://willmcgugan.com/timeline"
+
 
 class Notify(LogicElement):
 	"""Add a notification to the queue"""
-	xmlns = "http://willmcgugan.com/timeline"
+	xmlns = XMLNS
 
-	path = Attribute("Path to update", type="text")
-	action = Attribute("Action to send", type="text", required="yes")
-	data = Attribute("Data associated with the action", type="expression", default=None)
-	failsilently = Attribute("Ignore connectivity errors?", type="boolean", default=True)
+	path = Attribute("Path to update", type="text", required=True)
+	action = Attribute("Action to send", type="text", required=True)
 
 	def logic(self, context):
-		
-		path, action, data = self.get_parameters(context, 'path', 'action', 'data')
-		path = "/" + path.lstrip('/')
+		path, action = self.get_parameters(context, 'path', 'action')
 		log.debug('notify path="%s", action="%s"', path, action)
 
-		instruction = {"action": action}
-		if data:
-			instruction.update(data)
-		instruction.update(self.get_let_map(context))
+		instruction = self.get_let_map(context)
+		instruction['action'] = action
 		instruction_json = json.dumps([instruction])
 
 		notifications = context.set_new_call('.notifier_queue', list)
@@ -40,24 +36,23 @@ class Notify(LogicElement):
 
 class SendNotifications(LogicElement):
 	"""Send queued notifications"""
-	xmlns = "http://willmcgugan.com/timeline"
+	xmlns = XMLNS
 
 	def logic(self, context):
-		notifications = context.get('.notifier_queue', None)
+		notifications = context.pop('.notifier_queue', None)
 		if notifications is None:
 			return
 		log.debug('sending %s queued notifications', len(notifications))
 
-		timeline_app = self.archive.find_app('willmcgugan.timeline')
-		ws_url_base = timeline_app.settings['notifier_url']
-		notifier_secret = timeline_app.settings['notifier_secret']
-		ws_url = "{}?secret={}".format(ws_url_base, notifier_secret)
+		settings = self.archive.get_app_settings('willmcgugan.timeline')
+		ws_url = "{}?secret={}".format(settings['notifier_url'],
+		                               settings['notifier_secret'])
 
 		packet_json = json.dumps(notifications)
 		
 		sockopt = ((socket.IPPROTO_TCP, socket.TCP_NODELAY, 1),)
 		try:
-			ws = create_connection(ws_url, sockopt=sockop)
+			ws = create_connection(ws_url, sockopt=sockopt)
 		except Exception as e:
 			log.warn('unable to connect to notifier ({})'.format(text_type(e)))
 		else:
@@ -65,4 +60,3 @@ class SendNotifications(LogicElement):
 				ws.send(packet_json)
 			finally:
 				ws.close()
-		del context['.notifier_queue']
